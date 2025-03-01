483f753f62a054b432b2ec6c73b231a702b92fec
deep4109 <singhdeepankar41@gmail.com>
2025-03-01 15:23:13 +0530

    refresh path at runtine using refresh actuator

--- bank-microservice-accounts/src/main/java/com/example/bank_microservice_accounts/dto/AccountsContactInfoDto.java
+++ bank-microservice-accounts/src/main/java/com/example/bank_microservice_accounts/dto/AccountsContactInfoDto.java
@@ -1,11 +1,18 @@
 package com.example.bank_microservice_accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Setter
+@Getter
+public class AccountsContactInfoDto {
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 
 }
